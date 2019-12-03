c1e4f6171ef2f3d1cefae22d11160f81541fde5b
Jerome <jerome@metabolism.fr>
2019-12-03 01:57:14 -0800

    handle scaled down

--- fields/class-acf-field-image.php
+++ fields/class-acf-field-image.php
@@ -17,6 +17,13 @@ if( ! class_exists('acf_field_image_extension') ) :
                 'type'			=> 'text',
                 'placeholder'	=> 'ex: large:1920x1080:webp, small:150x150:jpg, medium:800x600'
             ));
+            acf_render_field_setting( $field, array(
+                'label'			=> __('Scaled down'),
+                'instructions'	=> __('Generate pre-rendered scaled down blurred version based on sizes parameter','acf'),
+                'name'			=> 'scaled_down',
+                'type'			=> 'true_false',
+	            'ui'			=> 1
+            ));
         }
 
 

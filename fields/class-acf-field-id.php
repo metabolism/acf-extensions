b53fc431bced7bf32fbe76a670812809437c495a
Jerome Barbato <jerome.barbato@gmail.com>
2022-08-22 22:20:36 +0200

    bugfix id

--- fields/class-acf-field-id.php
+++ fields/class-acf-field-id.php
@@ -24,9 +24,54 @@ if( ! class_exists('acf_field_id') ) :
 			$this->name = 'id';
 			$this->label = __("Uniqid",'acf');
 			$this->defaults = array(
-				'default_value'	=> uniqid()
+                'default_value'	=> ''
 			);
 		}
+
+        /*
+	     *  render_field()
+	     *
+	     *  Create the HTML interface for your field
+	     *
+	     *  @param	$field - an array holding all the field's data
+	     *
+	     *  @type	action
+	     *  @since	3.6
+	     *  @date	23/01/13
+	     */
+
+        function render_field( $field ) {
+
+            $input_attrs = array();
+            foreach( array( 'id', 'name', 'value', 'readonly', 'disabled' ) as $k ) {
+                if( isset($field[ $k ]) )
+                    $input_attrs[ $k ] = $field[ $k ];
+            }
+
+            $input_attrs['type'] = 'hidden';
+
+            echo acf_get_text_input( acf_filter_attrs($input_attrs) );
+        }
+
+        /*
+	     *  update_value()
+	     *
+	     *  Return uniqid on save
+	     *
+	     *  @param	$value - an array holding all the field's data
+	     *
+	     *  @type	action
+	     *  @since	3.6
+	     *  @date	23/01/13
+	     */
+
+        function update_value( $value ) {
+
+            if( empty($value) )
+                $value = uniqid();
+
+            return $value;
+        }
 	}
 
 	acf_register_field_type( 'acf_field_id' );

0e2561471fc235294ed10ffa0e8be70ce8726a65
Jerome Barbato <jerome.barbato@gmail.com>
2022-10-19 19:28:07 +0200

    bugfix

--- fields/class-acf-field-children.php
+++ fields/class-acf-field-children.php
@@ -27,8 +27,25 @@ if( ! class_exists('acf_field_children') ) :
         }
 
 
+		function render_field( $field ) {
+
+			$input_attrs = array();
+			foreach ( array( 'type', 'id', 'class', 'name', 'value', 'placeholder', 'maxlength', 'pattern', 'readonly', 'disabled', 'required' ) as $k ) {
+				if ( isset( $field[ $k ] ) ) {
+					$input_attrs[ $k ] = $field[ $k ];
+				}
+			}
+
+			$input_attrs['value'] = '[post_chidren]';
+			$html = '<div class="acf-input-wrap">' . acf_get_text_input( acf_filter_attrs( $input_attrs ) ) . '</div>';
+
+			// Display.
+			echo $html;
+		}
+
 		function format_value( $value, $post_id, $field ) {
 
+			$post_id = get_the_ID();
             $type = get_post_type($post_id);
             $query = new \WP_Query(['post_parent'=>$post_id, 'post_type'=>$type, 'posts_per_page'=>-1, 'order'=>'ASC', 'orderby'=>'menu_order']);
 
